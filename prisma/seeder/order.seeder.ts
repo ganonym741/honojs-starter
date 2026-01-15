b48e66528bc31f1fc58cfe0dbf0821983b4fe6ba
coffeecat <gunawan.anonym741@gmail.com>
2026-01-15 23:21:36 +0700

    fix: update order.seeder.ts

--- prisma/seeder/order.seeder.ts
+++ prisma/seeder/order.seeder.ts
@@ -1,17 +1,18 @@
-import { Order } from "@prisma/client";
-import { UserSeeds } from "./user.seeder";
+import Decimal from 'decimal.js';
+import { Order } from '../generated/client';
+import { UserSeeds } from './user.seeder';
 
 const OrderSeeds: Order[] = [
-    {
-        id: 'cmkesg6sd00052v759f1yfymz',
-        userId: UserSeeds[1].id,
-        orderNumber: `ORD-${Date.now()}-1`,
-        totalAmount: 1500000,
-        status: 'COMPLETED',
-        paymentStatus: 'PENDING',
-        paymentMethod: 'DOKU',
-        notes: `Sample order 1`,
-    }
-]
+  {
+    id: 'cmkesg6sd00052v759f1yfymz',
+    userId: UserSeeds[1].id,
+    orderNumber: `ORD-${Date.now()}-1`,
+    totalAmount: Decimal(1500000),
+    status: 'COMPLETED',
+    paymentStatus: 'PENDING',
+    paymentMethod: 'DOKU',
+    notes: `Sample order 1`,
+  },
+];
 
-export { OrderSeeds }
+export { OrderSeeds };
